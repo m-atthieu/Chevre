abc1401729656279085ccff86f57b7bfb73b040a
Matthieu DESILE <matthieu.desile@gmail.com>
2011-11-15 09:01:29 +0100

    add regexp support for MacOS X 10.6

--- Chevre/NSString+regex.m
+++ Chevre/NSString+regex.m
@@ -6,12 +6,18 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+
 #import "NSString+regex.h"
+
+#if MAC_OS_X_VERSION_MIN_REQUIRED <= MAC_OS_X_VERSION_10_6
+#import <regex.h>
+#endif
 
 @implementation NSString (regex)
 
-- (NSString *) captureRegex: (NSString *) pattern {
-    
+- (NSString *) captureRegex: (NSString *) pattern 
+{
+#if MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_6
     NSError *error = nil;
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionDotMatchesLineSeparators error:&error];
     if(regex == nil) {
@@ -19,15 +25,37 @@
         return nil;
     }
     
-    NSRange rangeOfFirstMatch = [regex rangeOfFirstMatchInString:self options:0 range:NSMakeRange(0, [self length])];
+    NSRange rangeOfFirstMatch = [regex rangeOfFirstMatchInString:self options: 0 range: NSMakeRange(0, [self length])];
     if(rangeOfFirstMatch.location == NSNotFound) return nil;
     
-    return [self substringWithRange:rangeOfFirstMatch];
+    return [self substringWithRange: rangeOfFirstMatch];
+#else 
+    regex_t preg;
+    int err = regcomp(&preg, [pattern UTF8String], REG_EXTENDED);
+    if(err){
+        char errbuf[256];
+        regerror(err, &preg, errbuf, sizeof(errbuf));
+        [NSException raise:@"CSRegexException"
+                    format:@"Could not compile regex '%@': %s", pattern, errbuf];
+    }
+    
+    const char *cstr = [self UTF8String];
+    regmatch_t match;
+    NSString* result;
+    if(regexec(&preg, cstr, 1, &match, 0) == 0)
+    {
+        result = [[[NSString alloc] initWithBytes: cstr + match.rm_so
+                                           length: match.rm_eo - match.rm_so 
+                                         encoding: NSUTF8StringEncoding] autorelease];
+    }
+    return result;
+#endif
 }
 
 
-- (BOOL)matchRegex:(NSString *)pattern {    
-    
+- (BOOL)matchRegex:(NSString *)pattern 
+{
+#if MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_6
     NSError *error = nil;
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern: pattern 
                                                                            options: NSRegularExpressionCaseInsensitive
@@ -39,5 +67,22 @@
     
     NSUInteger n = [regex numberOfMatchesInString:self options:0 range:NSMakeRange(0, [self length])];
     return n == 1;
+#else
+    regex_t preg;
+    int err = regcomp(&preg, [pattern UTF8String], REG_EXTENDED);
+    if(err){
+        char errbuf[256];
+        regerror(err, &preg, errbuf, sizeof(errbuf));
+        [NSException raise: @"CSRegexException"
+                    format: @"Could not compile regex '%@': %s", pattern, errbuf];
+    }
+    const char* cstr = [self UTF8String];
+    regmatch_t match;
+    if(regexec(&preg, cstr, 1, &match, 0) == 0){
+        return YES;
+    } else {
+        return NO;
+    }
+#endif
 }
 @end
