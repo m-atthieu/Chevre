133e8eaba616b5609e26e5f86a596c332f0b431f
Matthieu DESILE <matthieu.desile@gmail.com>
2012-06-08 08:59:49 +0200

    adding unit test target

---
+++ UnitTests/UnitTests.h
@@ -0,0 +1,13 @@
+//
+//  UnitTests.h
+//  UnitTests
+//
+//  Created by Matthieu DESILE on 6/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnitTests : SenTestCase
+
+@end
