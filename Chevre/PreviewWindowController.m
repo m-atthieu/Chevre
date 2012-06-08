319fba903c64ce8681d3038d7bf3fcfd23989d5e
Matthieu DESILE <matthieu.desile@gmail.com>
2012-06-08 08:58:23 +0200

    regexp are different between 10.6 and 10.7

--- Chevre/PreviewWindowController.m
+++ Chevre/PreviewWindowController.m
@@ -70,7 +70,11 @@
     for(NSDictionary* file in images){
         to = [file valueForKey: @"to"];
         directory = [to URLByDeletingLastPathComponent];
+#if MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_6
         [fm createDirectoryAtURL: directory withIntermediateDirectories: YES attributes: nil error: &error];
+#else
+        [fm createDirectoryAtPath: [directory path] withIntermediateDirectories: YES attributes: nil error: &error];
+#endif
         if(error != nil){ NSLog(@"error creating %@: %@", directory, [error localizedDescription]); continue; }
         [fm moveItemAtURL: [file valueForKey: @"from"] toURL: to error: &error];
         if(error != nil){ NSLog(@"error moving %@: %@", [file valueForKey: @"from"], [error localizedDescription]); continue; }
